f07d7d2cc42e8be6c837d47aa2ea419b40280d09
davidecesarano <davide.cesarano@unipegaso.it>
2016-10-29 16:37:29 +0200

    Aggiunto file Widgets.php
    Si occupa della configurazione dei widgets

---
+++ app/config/Widgets.php
@@ -0,0 +1,16 @@
+<?php 
+    
+    /**
+     * Configurazioni per i widget
+     * dell'applicazione
+     *
+     * @author Davide Cesarano 
+     */
+    
+    use Core\Config;
+    
+    Config::set('widgets', array(
+        
+    ));
+    
+    
