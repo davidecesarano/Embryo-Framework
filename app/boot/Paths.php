ac2941aa82e9d94917a270a9a16a5d552a32f4f6
davidecesarano <davide.cesarano@unipegaso.it>
2016-10-29 16:24:57 +0200

    Rimozione bug
    Rimosso bug per url con porta diversa da 80

--- app/boot/Paths.php
+++ app/boot/Paths.php
@@ -12,10 +12,10 @@
      * @const CURRENT_PAGE
      * ------------------------------------------------------------
      */
-
     define('SITE_PROTOCOL', ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://");
     define('SITE_FOLDER', dirname($_SERVER['PHP_SELF']));
-    define('SITE_URL', rtrim(SITE_PROTOCOL.$_SERVER['SERVER_NAME'].SITE_FOLDER, '/'));
+    define('SITE_PORT', $_SERVER['SERVER_PORT']);
+    define('SITE_URL', (SITE_PORT != 80) ? rtrim(SITE_PROTOCOL.$_SERVER['SERVER_NAME'].':'.SITE_PORT.SITE_FOLDER, '/') : rtrim(SITE_PROTOCOL.$_SERVER['SERVER_NAME'].SITE_FOLDER, '/'));
     define('SITE_BASE_DIR', getcwd());
     define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
     define('CURRENT_PAGE', SITE_PROTOCOL.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
