9f528631c96d4088fe93c3a8ee5c41a5bc7c7598
Davide Cesarano <davide.cesarano@unipegaso.it>
2016-11-17 10:24:29 +0100

    Inserimento meta_language() e meta_charset()

--- app/templates/public/default/views/header.php
+++ app/templates/public/default/views/header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo meta_language(); ?>">
 
 	<head>
-		<meta charset="utf-8">
+		<meta charset="<?php echo meta_charset(); ?>">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo $title; ?></title>
