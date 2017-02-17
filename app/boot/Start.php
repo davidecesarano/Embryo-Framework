0658f6faf0a3e22bede76d6c5bddd6ca1e13d905
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-02-17 17:33:14 +0100

    Modificato boot dell'applicazione

--- app/boot/Start.php
+++ app/boot/Start.php
@@ -1,32 +1,14 @@
 <?php
+
+    use Core\Config;
     
     /**
      * ------------------------------------------------------------
-     * PATHS	
+     * CONFIGURAZIONI	
      * ------------------------------------------------------------
      */
      
-    require 'Paths.php';
-    
-    /**
-     * ------------------------------------------------------------
-     * CARTELLE	
-     * ------------------------------------------------------------
-     */
-     
-    require 'Folders.php';
-    
-    /**
-     * ------------------------------------------------------------
-     * AUTOLOAD PSR-4	
-     * ------------------------------------------------------------
-     */
-     
-    if(!file_exists(FOLDER_VENDOR.'/autoload.php')){
-        die('File autoload.php non presente nella cartella vendor!');
-    }else{
-        require FOLDER_VENDOR.'/autoload.php';
-    }
+    Config::set();
     
     /**
      * ------------------------------------------------------------
@@ -35,16 +17,23 @@
      */
      
     require 'Functions.php';
-    
+
     /**
      * ------------------------------------------------------------
      * AMBIENTE DI SVILUPPO	
      * ------------------------------------------------------------
      */
     
-    require FOLDER_CONFIG.'/Error.php';
     enviroment();
     
+    /**
+     * ------------------------------------------------------------
+     * FUSO ORARIO
+     * ------------------------------------------------------------
+     */
+     
+    setTimezone();
+
     /**
      * ------------------------------------------------------------
      * GESTIONE DEGLI ERRORI
@@ -54,22 +43,7 @@
     register_shutdown_function('Core\Error::shutdownHandler');
     set_error_handler('Core\Error::errorHandler');
     set_exception_handler('Core\Error::exceptionHandler');
-    
-    /**
-     * ------------------------------------------------------------
-     * CONFIG	
-     * ------------------------------------------------------------
-     */
-     
-    require FOLDER_CONFIG.'/Database.php';
-    require FOLDER_CONFIG.'/Email.php';
-    require FOLDER_CONFIG.'/App.php';
-    require FOLDER_CONFIG.'/Api.php';
-    require FOLDER_CONFIG.'/Alias.php';
-    require FOLDER_CONFIG.'/Middlewares.php';
-    require FOLDER_CONFIG.'/Widgets.php';
-    require FOLDER_CONFIG.'/Session.php';
-    
+
     /**
      * ------------------------------------------------------------
      * ALIAS CLASSI 'HELPERS'
