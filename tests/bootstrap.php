fcf049504f91af346a74b7fba9566ea2f10c8f70
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-03-24 10:27:11 +0100

    Risoluzione problema versioni PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,7 @@
         }
     }
     spl_autoload_register('loader');
+    
+    if(!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')){
+        class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    }
