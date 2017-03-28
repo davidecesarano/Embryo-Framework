7a8367daafa1566132d57a5ec11ca32be8aa892e
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-03-28 11:19:31 +0200

    Risolti bug classe Helpers\Validation

--- app/helpers/Validation.php
+++ app/helpers/Validation.php
@@ -17,21 +17,21 @@
          * @var array $patterns
          */
         public $patterns = array(
-            'uri' 			=> '[A-Za-z0-9-\/_]+',
-            'url'			=> '[A-Za-z0-9-:.\/_]+',
-            'alpha'			=> '[A-Za-z]+',
-            'words'			=> '[\p{L}\s]+',
-            'alphanum'		=> '[A-Za-z0-9]+',
-            'int'			=> '[0-9]+',
-            'float'			=> '[[0-9\.,]+',
-            'tel'			=> '[0-9+\s()-]+',
-            'text'			=> '[\p{L}0-9\s-.,;:!"%&()?\'°#]+',
-            'file'			=> '[A-Za-z0-9-_]+\.[A-Za-z0-9]{2,4}',
-            'folder'		=> '[A-Za-z0-9-_]+',
-            'address'		=> '[\p{L}0-9\s.,()°-]+',
-            'date_dmy'		=> '[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}',
-            'date_ymd'		=> '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}',
-            'email'			=> '[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+'
+            'uri'           => '[A-Za-z0-9-\/_?&=]+',
+            'url'           => '[A-Za-z0-9-:.\/_?&=#]+',
+            'alpha'         => '[\p{L}]+',
+            'words'         => '[\p{L}\s]+',
+            'alphanum'      => '[\p{L}0-9]+',
+            'int'           => '[0-9]+',
+            'float'         => '[0-9\.,]+',
+            'tel'           => '[0-9+\s()-]+',
+            'text'          => '[\p{L}0-9\s-.,;:!"%&()?+\'°#\/@]+',
+            'file'          => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+\.[A-Za-z0-9]{2,4}',
+            'folder'        => '[\p{L}\s0-9-_!%&()=\[\]#@,.;+]+',
+            'address'       => '[\p{L}0-9\s.,()°-]+',
+            'date_dmy'      => '[0-9]{1,2}\-[0-9]{1,2}\-[0-9]{4}',
+            'date_ymd'      => '[0-9]{4}\-[0-9]{1,2}\-[0-9]{1,2}',
+            'email'         => '[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+'
         );
         
         /**
@@ -87,9 +87,19 @@
          */
         public function pattern($name){
             
-            $regex = '/^('.$this->patterns[$name].')$/u';
-            if($this->value != '' && !preg_match($regex, $this->value)){
-                $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+            if($name == 'array'){
+                
+                if(!is_array($this->value)){
+                    $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+                }
+            
+            }else{
+            
+                $regex = '/^('.$this->patterns[$name].')$/u';
+                if($this->value != '' && !preg_match($regex, $this->value)){
+                    $this->errors[] = 'Formato campo '.$this->name.' non valido.';
+                }
+                
             }
             return $this;
             
@@ -216,11 +226,21 @@
          */
         public function ext($extension){
 
-            if($this->file['error'] != 4 && pathinfo($this->file['name'], PATHINFO_EXTENSION) != $extension){
+            if($this->file['error'] != 4 && pathinfo($this->file['name'], PATHINFO_EXTENSION) != $extension && strtoupper(pathinfo($this->file['name'], PATHINFO_EXTENSION)) != $extension){
                 $this->errors[] = 'Il file '.$this->name.' non è un '.$extension.'.';
             }
             return $this;
             
+        }
+        
+        /**
+         * Purifica per prevenire attacchi XSS
+         *
+         * @param string $string
+         * @return $string
+         */
+        public function purify($string){
+            return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
         }
         
         /**
