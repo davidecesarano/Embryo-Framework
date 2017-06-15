fb050d402f8f6513b306ff13834d677e75aa9822
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-06-15 14:36:10 +0200

    Aggiunte rotte per la traduzione

--- app/Routes.php
+++ app/Routes.php
@@ -10,6 +10,12 @@
     $router->get('', 'Page@index');
     
     /**
+     * Traduzioni
+     */
+    $router->get('it', ['middleware' => 'Language:it', 'Page@index']);
+    $router->get('en', ['middleware' => 'Language:en', 'Page@index']);
+    
+    /**
      * Page
      */
     $router->get('example', 'Page@example');
