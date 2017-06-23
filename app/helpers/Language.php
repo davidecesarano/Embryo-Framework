ebf059dc48b038a209abf87ef806b9c94338816b
Davide Cesarano <davide.cesarano@unipegaso.it>
2017-06-23 12:17:08 +0200

    Risolto bug

--- app/helpers/Language.php
+++ app/helpers/Language.php
@@ -39,7 +39,7 @@
             self::setMessages(self::get());
 
             // lingua per le date
-            setlocale(LC_TIME, $locale, $encode);
+            setlocale(LC_TIME, $code, $encode);
 
         }
 
@@ -63,7 +63,7 @@
             self::setMessages($lang);
 
             // lingua per le date
-            setlocale(LC_TIME, $lang, $encode);
+            setlocale(LC_TIME, $code, $encode);
 
         }
 
