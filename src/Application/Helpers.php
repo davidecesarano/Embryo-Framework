1ebeaea83bca462c3d44e5b337e48245c5f9d670
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-17 17:36:22 +0100

    Add helpers
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- src/Application/Helpers.php
+++ src/Application/Helpers.php
@@ -2,10 +2,24 @@
 
     use Embryo\Application\Facades\Container;
 
+    function debug($data)
+    {
+        echo '<pre>';
+            print_r($data);
+        echo '</pre>';
+    }
+
     function site_url($path = null)
     {
         $url = Container::get('baseUrl');
         return (!$path) ? $url : $url.'/'.trim($path, '/');
+    }
+
+    function app_path($path = null) 
+    {
+        $settings = Container::get('settings');
+        $app_path = rtrim($settings['app']['root_path'], '/'); 
+        return (!$path) ? $app_path : $app_path.'/'.trim($path, '/').'/';
     }
 
     function template_path($path = null)
@@ -13,4 +27,12 @@
         $settings      = Container::get('settings');
         $template_path = $settings['view']['path']; 
         return (!$path) ? $template_path : $template_path.'/'.trim($path, '/');
+    }
+
+    function csrf_token()
+    {
+        $request = Container::get('request');
+        $session = $request->getAttribute('session');
+        $tokens  = $session->get('csrf_token', []);
+        return end($tokens);
     }
