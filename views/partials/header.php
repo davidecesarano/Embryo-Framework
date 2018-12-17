d9b2143acc7e35dbb90eb1af6079c8352606933c
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-17 14:03:06 +0100

    Fix favicon
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,6 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="shortcut icon" href="#">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{ $css }}" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
