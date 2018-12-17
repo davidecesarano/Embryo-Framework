8a500cc5bc22c6287452afcb1abc3c9c226ff06a
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-17 17:39:06 +0100

    Add middlewares
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

---
+++ bootstrap/middleware.php
@@ -0,0 +1,40 @@
+<?php
+    
+    /*
+    |--------------------------------------------------------------------------
+    | PSR-15 Middleware
+    |--------------------------------------------------------------------------
+    */
+
+    $container = $app->getContainer();
+    $settings  = $container['settings'];
+    $error     = $container['errorHandler'];
+    $session   = $settings['session'];
+    $router    = $container['router'];
+
+    // secure headers middleware
+    $app->addMiddleware(App\Middleware\SecureHeadersMiddleware::class);
+
+    // error middleware
+    $app->addMiddleware(
+        (new Embryo\Error\Middleware\ErrorHandlerMiddleware)
+            ->setErrorHandler($error)
+    );
+
+    // session middleware
+    $app->addMiddleware(
+        (new Embryo\Session\Middleware\SessionMiddleware)
+            ->setName($session['name'])
+            ->setOptions($session['options'])
+    );
+
+    // csrf middleware
+    $app->addMiddleware(Embryo\CSRF\CsrfMiddleware::class);
+
+    // minify html
+    $app->addMiddleware(Embryo\View\Middleware\MinifyHtmlMiddleware::class);
+
+    // routing middlewares
+    $app->addMiddleware(Embryo\Routing\Middleware\MethodOverrideMiddleware::class);
+    $app->addMiddleware(new Embryo\Routing\Middleware\RoutingMiddleware($router));
+    $app->addMiddleware(new Embryo\Routing\Middleware\RequestHandlerMiddleware($container));
