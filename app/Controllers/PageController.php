259d68af34d4cf04d6754c8f21b8c0ec44f93707
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-20 17:06:53 +0100

    Add phpdoc
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -1,11 +1,15 @@
 <?php 
     
+    /**
+     * PageController
+     */
+    
     namespace App\Controllers;
     
     use Embryo\Application\Controller;
-    
+
     class PageController extends Controller
-    {        
+    {   
         public function index($name)
         {
             $view = $this->get('view');
