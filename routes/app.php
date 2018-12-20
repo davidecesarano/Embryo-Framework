251fb746d6620c7eb9d98a2a3cce9d5065922528
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-20 17:07:30 +0100

    Rename controller route pattern
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- routes/app.php
+++ routes/app.php
@@ -1,10 +1,10 @@
 <?php
 
     $app->get('/', function($request, $response){
-        
+
         $view = $this->get('view');
         return $view->render($response, 'home', ['title' => 'Embryo 2']);
 
     });
 
-    $app->get('/{name}', 'PageController@index');
+    $app->get('/controller/{name}', 'PageController@index');
