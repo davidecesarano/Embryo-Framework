68b09cce2e5eafc88f1bebd8b99b1c83fbea9fad
davidecesarano <davide.cesarano@unipegaso.it>
2019-03-27 11:41:54 +0100

    Add bodyparser in ServerRequestService
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- Embryo/Services/ServerRequestService.php
+++ Embryo/Services/ServerRequestService.php
@@ -7,7 +7,7 @@
     namespace Embryo\Services;
 
     use Embryo\Container\ServiceProvider;
-    use Embryo\Http\Factory\ServerRequestFactory;
+    use Embryo\Http\Factory\{ServerRequestFactory, StreamFactory};
     
     class ServerRequestService extends ServiceProvider
     {
@@ -19,7 +19,23 @@
         public function register()
         {
             $this->container->set('request', function(){
-                return (new ServerRequestFactory)->createServerRequestFromServer();
+                
+                $request     = (new ServerRequestFactory)->createServerRequestFromServer();
+                $contentType = $request->getHeaderLine('Content-Type');
+
+                if ($contentType !== '' && strpos($contentType, 'application/json') !== false) {
+
+                    $stream = fopen('php://temp', 'w+');
+                    stream_copy_to_stream(fopen('php://input', 'r'), $stream);
+                    rewind($stream);
+                    
+                    $body    = (new StreamFactory)->createStreamFromResource($stream);
+                    $params  = json_decode($body->getContents(), true);
+                    $request = $request->withBody($body)->withParsedBody($params);
+
+                }
+                return $request;
+
             });
         }
     }
